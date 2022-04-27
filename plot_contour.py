7932fc9304b0d71c5315bd98820bbd13250598ce
dagrawa2 <dagrawa2@vols.utk.edu>
2022-04-27 14:40:18 -0400

    Add titles to contour plots.

--- plot_contour.py
+++ plot_contour.py
@@ -5,11 +5,11 @@ import numpy as np
 import matplotlib
 #matplotlib.use("agg")
 import matplotlib.pyplot as plt
-matplotlib.rc("xtick", labelsize=10)
-matplotlib.rc("ytick", labelsize=10)
+#matplotlib.rc("xtick", labelsize=10)
+#matplotlib.rc("ytick", labelsize=10)
 
 
-def plot(m, tau, angle, filename):
+def plot(m, tau, angle, filename, title=None, ylabel=None):
 	n = m if tau==0 else m//2
 	ks = np.arange(n)
 	w_x = np.cos(2*np.pi*ks/m + angle)
@@ -27,22 +27,27 @@ def plot(m, tau, angle, filename):
 	plt.contour(x, y, z, levels=20)
 	plt.quiver(zeros, zeros, w_x, w_y, color="blue")
 	plt.quiver(0, 0, c[0], c[1], color="red")
+	if title is not None:
+		plt.title(title, fontsize=8)
+	if ylabel is not None:
+		plt.ylabel(ylabel, fontsize=8)
+	plt.tight_layout()
 	plt.savefig(filename)
 	plt.close()
 
 
 # C_6
 os.makedirs("plots/vis/C_6/contour", exist_ok=True)
-plot(6, 0, 0, "plots/vis/C_6/contour/type1.png")
-plot(6, 1, 0, "plots/vis/C_6/contour/type2.png")
+plot(6, 0, 0, "plots/vis/C_6/contour/type1.png", title="Type 1")
+plot(6, 1, 0, "plots/vis/C_6/contour/type2.png", title="Type 2")
 
 # D_6
 os.makedirs("plots/vis/D_6/contour", exist_ok=True)
-plot(12, 0, 0, "plots/vis/D_6/contour/K0_type1.png")
-plot(12, 1, 0, "plots/vis/D_6/contour/K0_type2.png")
-plot(6, 0, np.pi/12, "plots/vis/D_6/contour/K1_type1.png")
+plot(12, 0, 0, "plots/vis/D_6/contour/K0_type1.png", title=r"$\{e\}$", ylabel="Type 1")
+plot(12, 1, 0, "plots/vis/D_6/contour/K0_type2.png", ylabel="Type 2")
+plot(6, 0, np.pi/12, "plots/vis/D_6/contour/K1_type1.png", title=r"$\{e, t\}$")
 plot(6, 1, np.pi/12, "plots/vis/D_6/contour/K1_type2.png")
-plot(6, 0, -5*np.pi/12, "plots/vis/D_6/contour/K2_type1.png")
+plot(6, 0, -5*np.pi/12, "plots/vis/D_6/contour/K2_type1.png", title=r"$\{e, r^3t\}$")
 plot(6, 1, -5*np.pi/12, "plots/vis/D_6/contour/K2_type2.png")
 
 print("Done!")
