82f6829bc5056944f012fc8e250b3968a495a099
dagrawa2 <dagrawa2@vols.utk.edu>
2022-05-03 10:09:09 -0400

    Rm obsolete functions in permlib.

--- grapefruit/permlib.py
+++ grapefruit/permlib.py
@@ -46,28 +46,3 @@ def projection_matrix(perms, return_order=False):
 	if return_order:
 		return P, order
 	return P
-
-def binary_matrix(degree):
-	A = galois.GF2([list(tup) for tup in itertools.product([0, 1], repeat=degree)]).T
-	return A
-
-def binary_matrix2int_vector(A):
-	out = np.array([int("".join(row), 2) for row in np.array(A.T).astype(str)])
-	return out
-
-def zorbit_transversal(generators):
-	degree = len(generators[0])
-	binmatrix = binary_matrix(degree)
-	vertices = binary_matrix2int_vector(binmatrix)
-	neighbors = np.stack([binary_matrix2int_vector( Permutation(gen).action(binmatrix, galois_field=True) ) for gen in generators], 0)
-	adjacency_dict = {v: N for (v, N) in zip(vertices, neighbors.T)}
-	G = nx.Graph(adjacency_dict)
-	components = nx.connected_components(G)
-	repr_vertices = sorted([min(C) for C in components])
-	repr_vectors = []
-	for v in repr_vertices:
-		vec = list(bin(v)[2:])
-		vec = ["0"]*(degree-len(vec)) + vec
-		repr_vectors.append(vec)
-	repr_vectors = 1-2*np.array(repr_vectors, dtype=int)
-	return repr_vectors
