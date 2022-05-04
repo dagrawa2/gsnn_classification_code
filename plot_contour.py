d57d0bded9e4efba1dcb1e382f72806ee42005c0
dagrawa2 <dagrawa2@vols.utk.edu>
2022-05-03 21:45:04 -0400

    Number quivers and change titles of contour plots.

--- plot_contour.py
+++ plot_contour.py
@@ -22,9 +22,11 @@ def plot(m, tau, angle, filename, line=None, title=None, ylabel=None):
 	levels = np.linspace(0.01, z.max(), 20)
 	plt.figure()
 	plt.contour(x, y, z, levels=levels)
-	plt.quiver(b*w_x, b*w_y, w_x, w_y, color="blue")
+	plt.quiver(-b*w_x, -b*w_y, w_x, w_y, color="blue")
 	if tau == 1:
 		plt.quiver(0, 0, c[0], c[1], color="red")
+	for (i, xy) in enumerate((1.05-b)*W):
+		plt.annotate(str(i+1), xy, color="blue", fontsize=14, weight="bold")
 	if line is not None:
 		plt.plot([-2, 2], [-2*np.tan(line), 2*np.tan(line)], linestyle="dashed")
 	if title is not None:
@@ -43,11 +45,11 @@ plot(6, 1, 0, "plots/vis/C_6/contour/type2.png", title="Type 2")
 
 # D_6
 os.makedirs("plots/vis/D_6/contour", exist_ok=True)
-plot(12, 0, 0, "plots/vis/D_6/contour/K0_type1.png", line=np.pi/12, title=r"$\{e\}$", ylabel="Type 1")
-plot(12, 1, 0, "plots/vis/D_6/contour/K0_type2.png", line=np.pi/12, ylabel="Type 2")
-plot(6, 0, np.pi/12, "plots/vis/D_6/contour/K1_type1.png", line=np.pi/12, title=r"$\{e, t\}$")
-plot(6, 1, np.pi/12, "plots/vis/D_6/contour/K1_type2.png", line=np.pi/12)
-plot(6, 0, -5*np.pi/12, "plots/vis/D_6/contour/K2_type1.png", line=np.pi/12, title=r"$\{e, r^3t\}$")
-plot(6, 1, -5*np.pi/12, "plots/vis/D_6/contour/K2_type2.png", line=np.pi/12)
+plot(12, 0, 0, "plots/vis/D_6/contour/K0_type1.png", line=np.pi/12, title="Architecture 0.0", ylabel="Type 1")
+plot(12, 1, 0, "plots/vis/D_6/contour/K0_type2.png", line=np.pi/12, title="Architecture 1.1", ylabel="Type 2")
+plot(6, 0, np.pi/12, "plots/vis/D_6/contour/K1_type1.png", line=np.pi/12, title="Architecture 2.0")
+plot(6, 1, np.pi/12, "plots/vis/D_6/contour/K1_type2.png", line=np.pi/12, title="Architecture 4.2")
+plot(6, 0, -5*np.pi/12, "plots/vis/D_6/contour/K2_type1.png", line=np.pi/12, title="Architecture 3.0")
+plot(6, 1, -5*np.pi/12, "plots/vis/D_6/contour/K2_type2.png", line=np.pi/12, title="Architecture 4.3")
 
 print("Done!")
