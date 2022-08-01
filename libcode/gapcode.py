f719c978de1064fd9254d9e349b875472c6156a3
dagrawa2 <dagrawa2@vols.utk.edu>
2022-08-01 10:52:08 -0400

    Update path to module in gapcode.

--- libcode/gapcode.py
+++ libcode/gapcode.py
@@ -1,7 +1,7 @@
 import json
 import subprocess
 
-def generate_representations(group_generators, output_file, path_to_gap="/home/dagrawa2/gap/gap-4.11.1/bin/gap.sh", path_to_gapscript="grapefruit/gapcode.g"):
+def generate_representations(group_generators, output_file, path_to_gap="/home/dagrawa2/gap/gap-4.11.1/bin/gap.sh", path_to_gapscript="libcode/gapcode.g"):
 	input = "input := {};; output_file := \"{}\";; Read(\"{}\");; quit;".format( \
 		str(group_generators), output_file, path_to_gapscript)
 	sub = subprocess.Popen([path_to_gap, "-b"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
